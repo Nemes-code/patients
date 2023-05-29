cdddbab0b63aca008b6d9ee83e4127506e474f6d
Danish Malik: git config --global user.name Danish <danishahmed888@gmail.com>
2023-05-29 14:48:20 +0530

    First Commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+
+const Header = () => {
+  return (
+    <div id='header'>
+        <h1> Record Keeping </h1>
+    </div>
+  )
+}
+
+export default Header
